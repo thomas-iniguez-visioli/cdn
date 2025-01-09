0799c7f36cccbaa23c6600fa597fa841f703538a
Thomas INIGUEZ-VISIOLI <tiniguezvi@290070u.local>
2025-01-09 16:33:22 +0100

    Auto-commit : changements locaux

--- search.js
+++ search.js
@@ -27,7 +27,7 @@ var searchFunc = function (path, search_id) {
       // get the contents from search data
       console.log(xmlResponse)
       var datas =  xmlResponse.filter((x)=>{
-        return '' 
+        return ''.includes(".j") 
       })
         .map(function (p) {
            
