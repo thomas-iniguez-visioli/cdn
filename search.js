0eb4bcfe264a2e68c36dba4b6060fa89d58f5d54
Thomas INIGUEZ-VISIOLI <tiniguezvi@290070u.local>
2025-01-10 09:56:26 +0100

    Auto-commit : changements locaux

--- search.js
+++ search.js
@@ -241,7 +241,7 @@ console.log(CONFIG)
     path             : CONFIG.path,
     top_n_per_article: CONFIG.localsearch.top_n_per_article,
     unescape         : CONFIG.localsearch.unescape,
-    localsearch:{}
+    localsearch:{preload:false,}
   });
 
   const input = document.querySelector('.search-input');
